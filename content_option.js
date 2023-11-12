369fe1bd61ea4665c8449d0b49e448e0b1c5e6e7
Eka Nurul <145452281+ekaaaanrllllll@users.noreply.github.com>
2023-11-12 20:13:47 +0700

    Add files via upload

---
+++ content_option.js
@@ -0,0 +1,139 @@
+const logotext = "≽^•⩊•^≼";
+const meta = {
+    title: "UTS Promnet",
+    description: "Aloo Aku Eka Nurul, Mahasiswi jurusan Pendidikan Ilmu Komputer 2022",
+};
+
+const introdata = {
+    title: "Eka Nurul Baridah",
+    animated: {
+        first: "Pemograman Internet",
+        second: "Ujian Tengah Semester",
+        third: "Pendidikan Ilmu Komputer - A",
+    },
+    description: "Web ini dibuat untuk menuntaskan Tugas Ujian Tengah Semester dalam Mata Kuliah Pemograman Internet yang diampu oleh Bapak Erlangga, S.Kom., M.T.",
+    your_img_url: "/Me/awal.png"
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Hai ! Kenalin aku Eka, tapi biasa dipanggil Kei sama orang-orang terdekatku. Aku lahir di Tangerang, 15 April 2004. Sekarang aku berkuliah di Universitas Pendidikan Indonesia, tepatnya di jurusan Pendidikan Ilmu Komputer. Aku suka nonton film, jalan-jalan, dan jajan hehe itu yang paling bikin happy. Salam kenal yaa !",
+};
+const worktimeline = [{
+        jobtitle: "SDN Jatimekar 4",
+        date: "2012",
+    },
+    {
+        jobtitle: "SMPN 09 Kota Bekasi",
+        date: "2019",
+    },
+    {
+        jobtitle: "SMKN 01 Kota Bekasi",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Ngantukan",
+        value: 100,
+    },
+    {
+        name: "Jajan",
+        value: 90,
+    },
+    {
+        name: "Makan sambil tidur",
+        value: 75,
+    },
+];
+
+const dataportfolio = [{
+        img: "/Me/satu.jpg",
+        description: "Ini lagi jalan-jalan ke puncak",
+        link: "#",
+    },
+    {
+        img: "/Me/dua.jpg",
+        description: "",
+        link: "#",
+    },
+    {
+        img: "/Me/tiga.jpeg",
+        description: "Ini namanya tata, salah satu bestie terbaik aku ",
+        link: "#",
+    },
+    {
+        img: "/Me/empat.jpeg",
+        description: "Ngantuk tapi lagi di kereta",
+        link: "#",
+    },
+    {
+        img: "/Me/lima.jpeg",
+        description: "Lagi lagi panit :)",
+        link: "#",
+    },
+    {
+        img: "/Me/enam.jpeg",
+        description: "Salah satu foto fav aku waktu lagi p2m",
+        link: "#",
+    },
+
+    {
+        img: "/Me/tujuh.jpg",
+        description: "Mau mudik dulu say :) ",
+        link: "#",
+    },
+    {
+        img: "/Me/delapan.jpeg",
+        description: "Lucu kan hehe",
+        link: "#",
+    },
+    {
+        img: "/Me/sembilan.jpeg",
+        description: "Gatau ah malu",
+        link: "#",
+    },
+    {
+        img: "/Me/duabel.jpg",
+        description: "No comment deh",
+        link: "#",
+    },
+    {
+        img: "/Me/sebelas.jpeg",
+        description: "Ini apni, roommate aku, salah satu manusia favorite setelah baju item",
+        link: "#",
+    },
+    {
+        img: "/Me/sepuluh.jpeg",
+        description: "Ini ipuy namanya, iya si baju item",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ekanurul415@upi.edu",
+    YOUR_PHONE: "085721736024",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "hhttps://github.com/ekaaaanrllllll",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
